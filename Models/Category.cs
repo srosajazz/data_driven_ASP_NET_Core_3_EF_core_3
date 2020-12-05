fa1697f25eac8e6d58b2d982525daf2d4bca8562
Sergio Rosa <scarlosjazz@gmail.com>
2020-12-05 16:54:35 -0500

    category model

---
+++ Models/Category.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Shop.Models
+{
+  public class Category
+  {
+    [Key]
+
+    public int Id {get; set;}
+    [Required(ErrorMessage = "This field is require")]
+    [MaxLength(60, ErrorMessage = "This field must have between 3 & 60 Characters")]
+    [MinLength(3, ErrorMessage = "This field must have between 3 & 60 Characters")]
+    public string Title{ get; set;}
+  }
+
+}
