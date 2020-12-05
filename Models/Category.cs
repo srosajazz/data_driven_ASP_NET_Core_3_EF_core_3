04bef9da9ee1d4e298c564679ba12a94fb837bd1
Sergio Rosa <scarlosjazz@gmail.com>
2020-12-05 17:08:40 -0500

    product model

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,17 +1,15 @@
 using System.ComponentModel.DataAnnotations;
 
-
-namespace Shop.Models
+namespace Backoffice.Models
 {
-  public class Category
-  {
-    [Key]
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
 
-    public int Id {get; set;}
-    [Required(ErrorMessage = "This field is require")]
-    [MaxLength(60, ErrorMessage = "This field must have between 3 & 60 Characters")]
-    [MinLength(3, ErrorMessage = "This field must have between 3 & 60 Characters")]
-    public string Title{ get; set;}
-  }
-
+        [Required(ErrorMessage = "This field is require")]
+        [MaxLength(60, ErrorMessage = "This field must have between 3 & 60 Characters")]
+        [MinLength(3, ErrorMessage = "This field must have between 3 & 60 Characters")]
+        public string Title { get; set; }
+    }
 }
