6271c02cdc54984f79e67c6300cfcb9c381cc96f
Sergio Rosa <scarlosjazz@gmail.com>
2020-12-07 15:51:28 -0500

    chore: model validation

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -4,6 +4,7 @@ using Backoffice.Models;
 using Microsoft.AspNetCore.Mvc;
 
 
+
 // EndPoint => URL
 // http://localhost:5000
 // https://localhost: 5001 - Azure
@@ -28,25 +29,35 @@ public class CategoryController : ControllerBase
   }
 
 
-// POST
+  // POST
   [HttpPost]
   [Route("")]
-  public async Task<ActionResult<Category>> Post([FromBody] Category model)
+  public async Task<ActionResult<List<Category>>> Post([FromBody] Category model)
   {
-    return model;
+    if (!ModelState.IsValid)
+    {
+        return BadRequest(ModelState);
+    }
+    return Ok(model);
   }
 
-//PUT
+  //PUT
   [HttpPut]
   [Route("{id:int}")]
-  public  async Task<ActionResult<Category>> Put(int id, [FromBody]Category model)
+  public async Task<ActionResult<Category>> Put(int id, [FromBody]Category model)
   {
-    if (model.Id == id)
-    return Ok(model);
+     // Verifica se o ID informado é o mesmo do modelo
+            if (id != model.Id)
+                return NotFound(new { message = "Category not found" });
 
-    return NotFound();
+            // Verifica se os dados são válidos
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+    return Ok(model);
   }
-//DELETE
+
+  //DELETE
   [HttpDelete]
   [Route("{id:int}")]
   public  async Task<ActionResult<Category>> Delete()
